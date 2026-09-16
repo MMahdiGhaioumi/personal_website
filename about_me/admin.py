e74b9cc35e988f13bf7296d0bd926b86199a93dc
MohammadMahdiGhaioumi <mahdi.ghaioumi.office@gmail.com>
2026-09-16 16:45:42 +0330

    registring Service, Skill and Resume in about me app

--- about_me/admin.py
+++ about_me/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'show_image')
+
+    @admin.display(description="نگاره سروس")
+    def show_image(self, obj):
+        return format_html(
+            f'<div style="background: black; width: 200px; height: 80px; justify-content: center; align-items: center; display: flex;"><img src="{obj.icon.url}" width="300pxpx" height="80px"></div>')
+
+
+@admin.register(models.Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    radio_fields = {'place': admin.HORIZONTAL}
+
+
+admin.site.register(models.Skill)
