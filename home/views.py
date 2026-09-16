e03a874e97fa664d3eae5ebc55b81dcc5bc2aefd
MohammadMahdiGhaioumi <mahdi.ghaioumi.office@gmail.com>
2026-09-16 17:15:54 +0330

    creating HomeView for home app and showing its templates.

--- home/views.py
+++ home/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from typing import Any
+from django.contrib.auth import get_user_model
+from django.views.generic import TemplateView
+from contact import forms
+from about_me import models
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'home/index.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+
+        context['my_user'] = get_user_model().objects.last()
+
+        context['services'] = models.Service.objects.all()
+
+        context['skills'] = models.Skill.objects.all()
+
+        context['resume_education'] = models.Resume.objects.filter(place=models.Resume.Place.EDUCATION)
+        context['resume_experience'] = models.Resume.objects.filter(place=models.Resume.Place.EXPERIENCE)
+
+        context['form'] = forms.MessageForm()
+
+        return context
