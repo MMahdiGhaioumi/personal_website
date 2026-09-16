d97858b1c33b19b7359a10b7dd496ffeb2629237
MohammadMahdiGhaioumi <mahdi.ghaioumi.office@gmail.com>
2026-09-16 16:21:29 +0330

    adding mysql to database settings

--- config/settings.py
+++ config/settings.py
@@ -68,8 +68,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_site',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+        'OPTIONS': {}
     }
 }
 
