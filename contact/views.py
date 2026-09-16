3256979fa97ebeef3d9efa901231b93c28c155f5
MohammadMahdiGhaioumi <mahdi.ghaioumi.office@gmail.com>
2026-09-16 16:56:40 +0330

    creating view CreateMessage for creating message in models

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from . import forms
 
-# Create your views here.
+
+class CreateMessage(CreateView):
+    form_class = forms.MessageForm
+    template_name = 'home/index.html'
+    success_url = reverse_lazy('home:home')
