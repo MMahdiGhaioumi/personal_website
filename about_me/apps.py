f97fcde6587b44eb0947c81acda9b93b05beb066
MohammadMahdiGhaioumi <mahdi.ghaioumi.office@gmail.com>
2026-09-16 16:42:39 +0330

    creating verbose name for about me app

--- about_me/apps.py
+++ about_me/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AboutMeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'about_me'
+    verbose_name = 'بخش اطلاعات من'
