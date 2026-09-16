6371eff777199de17afb63b0b83651897de3debe
MohammadMahdiGhaioumi <mahdi.ghaioumi.office@gmail.com>
2026-09-16 17:13:23 +0330

    registring UserAdmin and adding ilines named SocialMediaTabular, PhoneTabular, AddressTabular and EmailTabular for UserAdmin. and Unregistering Gropu model.

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,100 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+from . import forms
 
-# Register your models here.
+from contact import models
+
+
+class SocialMediaTabular(admin.TabularInline):
+    model = models.SocialMedia
+    extra = 0
+    classes = ["collapse"]
+
+
+class PhoneTabular(admin.TabularInline):
+    model = models.Phone
+    extra = 0
+    classes = ["collapse"]
+
+
+class AddressTabular(admin.TabularInline):
+    model = models.Address
+    extra = 0
+    classes = ["collapse"]
+
+
+class EmailTabular(admin.TabularInline):
+    model = models.Email
+    extra = 0
+    classes = ["collapse"]
+
+
+@admin.register(get_user_model())
+class UserAdmin(UserAdmin):
+    form = forms.UserChangeForm
+    add_form = forms.UserCreationForm
+
+    list_display = ['username', 'first_name', 'last_name']
+    search_fields = ['username', 'user__emails']
+    filter_horizontal = []
+    list_filter = []
+    inlines = [
+        SocialMediaTabular,
+        PhoneTabular,
+        EmailTabular,
+        AddressTabular,
+    ]
+
+    fieldsets = [
+        ("حساب کاربری", {
+            "fields": [
+                'username',
+                'password',
+                'is_admin',
+            ]
+        }),
+        ("اطلاعات عمومی کاربر", {
+            "fields": [
+                ('first_name', 'last_name'),
+                ('country', 'city'),
+                'birth_date',
+                'age',
+                'image',
+                'resume',
+            ]
+        }),
+        ("توضیحات کوتاه و کامل درباره کاربر", {
+            "fields": [
+                'information',
+                'short_information'
+            ],
+            'classes': ['collapse']
+        }),
+        ("توضحات درباره مهارت ها", {
+            "fields": [
+                'title_skills',
+                'text_skills',
+            ],
+            'classes': ['collapse']
+        }),
+    ]
+
+    add_fieldsets = [
+        (None, {
+            'fields': [
+                'username',
+                'password1',
+                'password2',
+            ]
+        })
+    ]
+
+    def get_inlines(self, request, obj=None):
+        if obj is None:
+            return []
+        return self.inlines
+
+
+admin.site.unregister(Group)
