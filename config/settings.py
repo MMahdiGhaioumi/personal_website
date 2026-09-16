64441b4cc1f8a9c5f839cb119680533d5b0f6bab
MohammadMahdiGhaioumi <mahdi.ghaioumi.office@gmail.com>
2026-09-16 16:19:10 +0330

    adding staticfiles dirs, static root, media root and media url in settings.

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from django.conf.global_settings import STATICFILES_DIRS, STATIC_ROOT, MEDIA_ROOT, MEDIA_URL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -108,6 +110,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'assets',
+]
+STATIC_ROOT = BASE_DIR / 'statics'
+
+# Media files
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
