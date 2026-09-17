11e2ab286bdb8681189abb5981d65195b23427f5
MohammadMahdiGhaioumi <mahdi.ghaioumi.office@gmail.com>
2026-09-17 13:20:01 +0330

    loading dotenv after BaseDir and changing value of NAME key in DATABASES to getenv.

--- config/settings.py
+++ config/settings.py
@@ -7,12 +7,14 @@ from django.conf.global_settings import STATICFILES_DIRS, STATIC_ROOT, MEDIA_ROO
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# load dotenv
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("secret_key")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,11 +80,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'my_site',
-        'USER': os.getenv("db_user"),
-        'PASSWORD': os.getenv("db_password"),
-        'HOST': os.getenv("db_host"),
-        'PORT': os.getenv("db_port"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
         'OPTIONS': {}
     }
 }
