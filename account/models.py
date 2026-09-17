43febe34b51a573e68dd2ae4e65a75d98ddcc46b
MohammadMahdiGhaioumi <mahdi.ghaioumi.office@gmail.com>
2026-09-17 16:57:21 +0330

    changing get_full_name method in user model

--- account/models.py
+++ account/models.py
@@ -45,7 +45,7 @@ class User(AbstractBaseUser):
     def get_full_name(self):
         if self.first_name and self.last_name:
             return f"{self.first_name} {self.last_name}"
-        return self.username
+        return ''
 
     def is_superuser(self):
         return self.is_admin
